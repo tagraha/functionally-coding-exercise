06ce1c26e9b84901336f4a48e4037a09ec756025
Tirta Nugraha <dev.nugrata@gmail.com>
2019-04-13 18:58:06 +0700

    other(logger): adding redux logger

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,13 +1,18 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import rootReducer from './reducers'
+import { createLogger } from 'redux-logger'
 
+const logger = createLogger({
+  collapsed: true,
+});
 
 export default function configureStore(preloadedState) {
   return createStore(
     rootReducer,
     preloadedState,
     applyMiddleware(
+      logger,
       thunkMiddleware
     )
   )
